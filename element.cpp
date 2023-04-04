7dab6302773a252fe67b9b4ae4a258962a48fad1
tranBaoNgocj <tranbaongoc038@gmail.com>
2023-04-04 11:32:20 +0700

    04/04

--- element.cpp
+++ element.cpp
@@ -1,43 +1,14 @@
-#include <bits/stdc++.h>
 #include "element.h"
 using namespace std;
 
-struct barrier {
-    int height; int position;
-    barrier(int _height, int _position){
-        height = _height;
-        position = _position;
-    }
-    void printBar (barrier a){
-        for(int i=0;i<35;i++){
-            if(i==0 || i==34) cout<<"|";
-            else {
-                if(i>=a.position && i<=a.position+4) cout<<"_";
-                else cout<<" ";
-            }
-        }
-        cout<<endl;
-    }
-};
-
+// creat ball
 struct ball {
     int height;
     int position;
-    ball(int _height, int _position){
+    int currentBarrierPosition;
+    ball(int _height, int _position,int _currentBarrierPosition){
         height = _height;
         position = _position;
-    }
-    void printBall(ball a){
-        for(int i=0;i<35;i++){
-            if(i==a.position) cout<<"o";
-            else if(i==0 || i==34) cout<<"|";
-            else cout<<" ";
-        }
-        cout<<endl;
+        currentBarrierPosition = _currentBarrierPosition;
     }
 };
-
-void firstLine(){
-    string firstLine(35,'-');
-    cout<<firstLine<<endl;
-}
--- element.h
+++ element.h
@@ -1,1 +1,6 @@
-struct barrier{};
+#ifndef ELEMENT_H
+#define ELEMENT_H
+
+
+struct ball{int height;int position;int currentBarrierPosition;ball(int _height, int _position,int _currentBarrierPosition);};
+#endif
